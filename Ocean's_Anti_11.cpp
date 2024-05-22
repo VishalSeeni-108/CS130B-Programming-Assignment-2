edb1d7a564c25e58920103a7e76dc8f5a332260a
Viz108 <67886538+Viz108@users.noreply.github.com>
2024-05-22 12:57:29 -0700

    Substring count implemented, need to fix output

--- Ocean's_Anti_11.cpp
+++ Ocean's_Anti_11.cpp
@@ -1,32 +1,19 @@
 #include <iostream> 
 #include <vector> 
+#include <math.h>
 
 using namespace std;
 
 struct dpData //data passed up from previous cases
 {
     long long int numStrings; 
-    long long int numStartOne; 
     long long int numEndOne;
-    long long int numStartZero; 
-    long long int numEndZero;  
 }; 
 
 main()
 {
     int numTests; 
-    cin >> numTests; 
-    vector<dpData> dpTable; 
-    dpData dummyCase1; 
-    dpData dummyCase2; 
-    dpData baseCase; //substring of length 2 
-    baseCase.numStrings = 3;
-    baseCase.numStartOne = 1; 
-    baseCase.numEndOne = 1;
-    dpTable.push_back(dummyCase1);
-    dpTable.push_back(dummyCase2);   
-    dpTable.push_back(baseCase); 
-     
+    cin >> numTests;      
 
     for(int i = 0; i < numTests; i++)
     {
@@ -35,37 +22,26 @@ main()
         
         vector<dpData> dpTable; 
         dpData dummyCase1; 
-        dpData dummyCase2; 
-        dpData baseCase; //substring of length 2 
-        baseCase.numStrings = 3;
-        baseCase.numStartOne = 1; 
-        baseCase.numEndOne = 1;
-        baseCase.numStartZero = 1;
-        baseCase.numEndZero = 1; 
+        dpData oneCase; //substring of length 1
+        dpData twoCase; //substring of length 2 
+        oneCase.numStrings = 2; 
+        oneCase.numEndOne = 1; 
+        twoCase.numStrings = 3;
+        twoCase.numEndOne = 1;
         dpTable.push_back(dummyCase1);
-        dpTable.push_back(dummyCase2);   
-        dpTable.push_back(baseCase);
+        dpTable.push_back(oneCase);   
+        dpTable.push_back(twoCase);
         
         for(int j = 3; j <= length; j++) 
         {      
-           cout << "j: " << j << endl; 
-           long long int zeroAddedToEnd = (dpTable.at(j-1).numEndZero); 
-           cout << "zeroAddedToEnd: " << zeroAddedToEnd << endl;
-           long long int oneAddedToEnd = (dpTable.at(j-1).numStrings - dpTable.at(j-1).numEndOne);
-           cout << "oneAddedToEnd: " << oneAddedToEnd << endl;  
-           long long int zeroAddedToBeginning = dpTable.at(j-1).numStartZero;
-           cout << "zeroAddedToBeginning: " << zeroAddedToBeginning << endl;  
-           long long int oneAddedToBeginning = (dpTable.at(j-1).numStrings - dpTable.at(j-1).numStartOne);
-           cout << "oneAddedToBeginning: " << oneAddedToBeginning << endl; 
+           long long int addZero = dpTable.at(j-1).numStrings % (long long int)(pow(10,9) + 7); 
+           long long int addOne = ((dpTable.at(j-1).numStrings % (long long int)(pow(10,9) + 7)) - (dpTable.at(j-1).numEndOne % (long long int)(pow(10,9) + 7))) % (long long int)(pow(10,9) + 7); 
 
            dpData newCase; 
-           newCase.numStrings = zeroAddedToEnd + oneAddedToEnd + zeroAddedToBeginning + oneAddedToBeginning; 
-           newCase.numStartOne = oneAddedToBeginning; 
-           newCase.numEndOne = oneAddedToEnd; 
-           newCase.numStartZero = zeroAddedToBeginning; 
-           newCase.numEndZero = zeroAddedToEnd;  
-           dpTable.push_back(newCase);  
+           newCase.numStrings = ((addZero  % (long long int)(pow(10,9) + 7)) + (addOne % (long long int)(pow(10,9) + 7))) % (long long int)(pow(10,9) + 7); 
+           newCase.numEndOne = (addOne % (long long int)(pow(10,9) + 7)); 
+           dpTable.push_back(newCase); 
         }
-        cout << dpTable.at(length).numStrings << endl; 
+        cout << (dpTable.at(length).numStrings) << endl;
     }
 }
