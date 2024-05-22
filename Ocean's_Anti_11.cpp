b9b5b3bd4a1beba05953364441ab9649897cb613
Vishal S <vishalseeni@gmail.com>
2024-05-22 02:08:29 -0700

    Todo: Fix counting previous ss with start/end 0

--- Ocean's_Anti_11.cpp
+++ Ocean's_Anti_11.cpp
@@ -7,7 +7,9 @@ struct dpData //data passed up from previous cases
 {
     long long int numStrings; 
     long long int numStartOne; 
-    long long int numEndOne; 
+    long long int numEndOne;
+    long long int numStartZero; 
+    long long int numEndZero;  
 }; 
 
 main()
@@ -38,23 +40,30 @@ main()
         baseCase.numStrings = 3;
         baseCase.numStartOne = 1; 
         baseCase.numEndOne = 1;
+        baseCase.numStartZero = 1;
+        baseCase.numEndZero = 1; 
         dpTable.push_back(dummyCase1);
         dpTable.push_back(dummyCase2);   
         dpTable.push_back(baseCase);
         
         for(int j = 3; j <= length; j++) 
         {      
-    
-           long long int zeroAddedToEnd = dpTable.at(j-1).numStrings; 
-           long long int oneAddedToEnd = (dpTable.at(j-1).numStrings - dpTable.at(j-1).numEndOne); 
-           long long int zeroAddedToBeginning = dpTable.at(j-1).numStrings; 
+           cout << "j: " << j << endl; 
+           long long int zeroAddedToEnd = (dpTable.at(j-1).numEndZero); 
+           cout << "zeroAddedToEnd: " << zeroAddedToEnd << endl;
+           long long int oneAddedToEnd = (dpTable.at(j-1).numStrings - dpTable.at(j-1).numEndOne);
+           cout << "oneAddedToEnd: " << oneAddedToEnd << endl;  
+           long long int zeroAddedToBeginning = dpTable.at(j-1).numStartZero;
+           cout << "zeroAddedToBeginning: " << zeroAddedToBeginning << endl;  
            long long int oneAddedToBeginning = (dpTable.at(j-1).numStrings - dpTable.at(j-1).numStartOne);
-
+           cout << "oneAddedToBeginning: " << oneAddedToBeginning << endl; 
 
            dpData newCase; 
            newCase.numStrings = zeroAddedToEnd + oneAddedToEnd + zeroAddedToBeginning + oneAddedToBeginning; 
            newCase.numStartOne = oneAddedToBeginning; 
            newCase.numEndOne = oneAddedToEnd; 
+           newCase.numStartZero = zeroAddedToBeginning; 
+           newCase.numEndZero = zeroAddedToEnd;  
            dpTable.push_back(newCase);  
         }
         cout << dpTable.at(length).numStrings << endl; 
