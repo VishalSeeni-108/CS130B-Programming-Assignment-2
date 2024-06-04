d1a73b9a45aa1066986d85342b7648a21a104c0c
Vishal Seenivasan <vseenivasan@ucsb.edu>
2024-06-03 19:57:19 -0700

    Final commit for problem C - passing all tests

--- Train_Sorting.cpp
+++ Train_Sorting.cpp
@@ -5,66 +5,77 @@
 
 using namespace std; 
 
-main()
+int main()
 {
     int numCars; 
     cin >> numCars; 
 
-    vector<list<int>> dynamicTable; 
+    vector<int> cars; 
 
     for(int i = 0; i < numCars; i++)
     {
         int newCarWeight; 
         cin >> newCarWeight;
-
-        vector<list<int>> tempTable; 
-        for(int i = 0; i < dynamicTable.size(); i++)
-        {
-            //Don't add new car
-            tempTable.push_back(dynamicTable.at(i)); 
-            //Add at back, if possible
-            if(newCarWeight < dynamicTable.at(i).front())
-            {
-                list<int> tempOrder= dynamicTable.at(i); 
-                tempOrder.push_front(newCarWeight);   
-                tempTable.push_back(tempOrder); 
-            }
-            //Add at front, if possible
-            if(newCarWeight > dynamicTable.at(i).back())
-            {
-                list<int> tempOrder = dynamicTable.at(i); 
-                tempOrder.push_back(newCarWeight); 
-                tempTable.push_back(tempOrder); 
-            }      
-        }
-        //Add new car as starting point 
-        tempTable.push_back({newCarWeight});
-
-        //Replace old dynamic table with temp table
-        dynamicTable.clear(); 
-        dynamicTable = tempTable; 
-
-        // cout << "After adding " << newCarWeight << ": " << endl; 
-        // for(int i = 0; i < dynamicTable.size(); i++)
-        // {
-        //     for(int n : dynamicTable.at(i))
-        //     {
-        //         cout << n;
-        //     }
-        //     cout << endl; 
-        // }
+        cars.push_back(newCarWeight); 
     }
 
-    //Output longest list as longest possible train
-    // cout << "Final dynamic table: " << endl; 
-    // for(int i = 0; i < dynamicTable.size(); i++)
+    //Find longest increasing substring at each index
+    vector<int> longestIncreasingSubstring(cars.size(), 1); 
+    for(int i = cars.size() - 2; i >= 0; i--)
+    {
+        for(int j = i; j < cars.size(); j++)
+        {
+            if(cars.at(j) > cars.at(i))
+            {
+                int potentialNewLongest = longestIncreasingSubstring.at(j) + 1; 
+                if(potentialNewLongest > longestIncreasingSubstring.at(i))
+                {
+                    longestIncreasingSubstring.at(i) = potentialNewLongest;  
+                }
+            }
+        }
+    }
+
+    //Find longest decreasing substring at each index
+    vector<int> longestDecreasingSubstring(cars.size(), 1); 
+    for(int i = cars.size() - 2; i >= 0; i--)
+    {
+        for(int j = i; j < cars.size(); j++)
+        {
+            if(cars.at(j) < cars.at(i))
+            {
+                int potentialNewLongest = longestDecreasingSubstring.at(j) + 1; 
+                if(potentialNewLongest > longestDecreasingSubstring.at(i))
+                {
+                    longestDecreasingSubstring.at(i) = potentialNewLongest;  
+                }
+            }
+        }
+    }
+
+    //Calculate longest possible train at each index and take max
+    vector<int> longestTrain(cars.size(), 1); 
+    for(int i = 0; i < longestTrain.size(); i++)
+    {
+        longestTrain.at(i) = longestIncreasingSubstring.at(i) + longestDecreasingSubstring.at(i) - 1; 
+    }
+
+    if(longestTrain.size() != 0)
+    {
+        cout << *max_element(longestTrain.begin(), longestTrain.end()) << endl; 
+    }
+    else
+    {
+        cout << 0 << endl; 
+    }
+    
+    // for(int i = 0; i < longestIncreasingSubstring.size(); i++)
     // {
-    //     for(int n : dynamicTable.at(i))
-    //     {
-    //         cout << n;
-    //     }
-    //     cout << endl; 
+    //     cout << "Longest increasing substring starting at " << cars.at(i) << ": " << longestIncreasingSubstring.at(i) << endl; 
     // }
 
-    cout << (*(max_element(dynamicTable.begin(), dynamicTable.end(), [](list<int> a, list<int> b){return b.size() > a.size();}))).size() << endl; 
+    // for(int i = 0; i < longestDecreasingSubstring.size(); i++)
+    // {
+    //     cout << "Longest decreasing substring starting at " << cars.at(i) << ": " << longestDecreasingSubstring.at(i) << endl; 
+    // }
 }
